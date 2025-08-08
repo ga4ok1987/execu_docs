ba7e807bdad60e70d020b73f852ec45a036d1569
ga4ok1987 <107840293+ga4ok1987@users.noreply.github.com>
2025-08-08 22:08:47 +0300

    Delete lib/data/datasources/local/region_dao.g.dart

--- lib/data/datasources/local/region_dao.g.dart
+++
@@ -1,9 +0,0 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
-
-part of 'region_dao.dart';
-
-// ignore_for_file: type=lint
-mixin _$RegionDaoMixin on DatabaseAccessor<AppDatabase> {
-  $RegionsTable get regions => attachedDatabase.regions;
-  $ExecutorOfficesTable get executorOffices => attachedDatabase.executorOffices;
-}
